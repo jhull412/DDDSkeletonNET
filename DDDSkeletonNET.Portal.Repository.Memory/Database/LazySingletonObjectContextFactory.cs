487fb1d85926c0a9a9a622f20234c9a23eb32c6d
Justin Hull <hulljc3@upmc.edu>
2016-03-29 11:40:05 -0400

    Finished Part 5 http://dotnetcodr.com/2013/09/26/a-model-net-web-service-based-on-domain-driven-design-part-5-the-concrete-repository/

---
+++ DDDSkeletonNET.Portal.Repository.Memory/Database/LazySingletonObjectContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDSkeletonNET.Portal.Repository.Memory.Database
+{
+    public class LazySingletonObjectContextFactory : IObjectContextFactory
+    {
+        public InMemoryDatabaseObjectContext Create()
+        {
+            return InMemoryDatabaseObjectContext.Instance;
+        }
+    }
+}
